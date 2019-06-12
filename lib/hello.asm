b059cc0cadba5e11b1d9647fabd2d861ccaad8c1
William Diebolt <wmdiebolt@gmail.com>
2019-06-12 14:12:16 -0700

    print_uint seems to work

--- lib/hello.asm
+++ lib/hello.asm
@@ -1,16 +1,20 @@
 global _start
 
 section .data
-message: db 'hello, naughty naughty!', 10
+message: db 'aye sup dawg!', 10
 
 section .text
 
 _start:
+	mov rdi, 65
+	call print_char
+	call print_newline
 	
 	mov rdi, message	; rdi <- arg for str_len
-	call string_length	; rax <- return val
-	mov rsi, rax		; rsi gets length (rdi still pts to msg)
 	call print_string
+	mov rdi, 1234
+	call print_uint
+	call print_newline
 	jmp exit
 
 exit:
@@ -28,10 +32,60 @@ string_length:
 	ret
 
 print_string:
-	mov rdx, rsi
+	call string_length
+	mov rdx, rax		; length arg moved for syscall		
+	mov rsi, rdi		; string ptr moved for syscall
 	mov rax, 1			; 'write' syscall numb
 	mov rdi, 1			; stdout descriptor
-	mov rsi, message	; string addr	
 	syscall
 	ret				
 
+print_char:
+	push rdi			; place arg to print on stack
+	mov rsi, rsp		; pointer to the value on stack
+	mov rdx, 1			; length to write
+	mov rdi, 1			; file descriptor where to write
+	mov rax, 1			; syscall write code
+	syscall
+	pop rdi
+	ret
+
+print_newline:
+	mov rax, 1
+	mov rdi, 1
+	mov rdx, 1
+	push 10
+	mov rsi, rsp
+	syscall
+	pop rdi
+	ret
+
+print_uint:
+	;handle initial zero! right now nothin would print
+
+	mov rax, rdi		;place arg into diff register
+	xor rbx, rbx		;count in rbx from zero
+	mov rdi, 10			;need 10 in a register 
+  .fillbuf:
+	cmp rax, 0			;if rax is zero, 
+	je .revprint			;jump to print
+	xor rdx, rdx		;zero
+	div rdi			; rax <- qoutient, rdx <- rem 
+	add rdx, '0'		; transform digit to ASCII code
+	push rdx			; ASCII digits stored in reverse
+	inc rbx				
+	jmp .fillbuf
+  .revprint:
+	cmp rbx, 0
+	je .end
+	dec rbx	
+	mov rsi, rsp
+	mov rdi, 1
+	mov rdx, 1
+	mov rax, 1
+	syscall
+	pop rax
+	jmp .revprint
+
+  .end:
+	ret
